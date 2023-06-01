3dfa9959854a07b28bba7302a841e4ea713536be
JoaoEnesGoncalves <joao.goncalves@hitachivantara.com>
2023-06-01 18:16:38 +0100

    feat(test): ui script to filter report by text type

--- Pentaho/tests/spec/AnalyzerFilterFromList.spec.ts
+++ Pentaho/tests/spec/AnalyzerFilterFromList.spec.ts
@@ -1,12 +1,15 @@
-import { expect, test } from '@playwright/test'
+import { test } from '@playwright/test'
 import { Report } from '../../src/api/analyzer/Report'
 import { EnvConst } from '../../src/environment/EnvConst'
 import { ReportEditMode } from '../../src/pages/analyzer/ReportEditMode'
+import { ReportTableSteps } from '../../src/pages/analyzer/ReportTableSteps'
 
 
 const pucReportPath = '/home/admin/testReportx'
 const report = new Report
 const resourceReport = EnvConst.RESOURCES_DIRECTORY + 'reports/TerritorySales.xml'
+const filters = ["Territory excludes APAC, Japan, NA and Not Available",
+    "Territory includes EMEA"]
 
 
 test('Filter select from a list - includes vs excludes', async ({ page }) => {
@@ -14,10 +17,18 @@ test('Filter select from a list - includes vs excludes', async ({ page }) => {
         await report.createReport(pucReportPath, resourceReport)
     })
 
-    await test.step(`UI script to open an analyzer report ${pucReportPath}`, async () => {
-        const reportEditMode = new ReportEditMode(page, pucReportPath)
-        await reportEditMode.goTo()
-    })
+    for (const index in filters) {
+        await test.step(`Given Admin user opened the Analyzer Report ${pucReportPath}"`, async () => {
+            const reportEditMode = new ReportEditMode(page, pucReportPath)
+            await reportEditMode.goTo()
+        })
+
+        await test.step(`When Admin user filter the report by <FILTER>: ${filters[index]}`, async () => {
+            const tableSteps = new ReportTableSteps(page)
+            await tableSteps.setFilterAs(filters[index])
+        })
+    }
+
 })
 
 
