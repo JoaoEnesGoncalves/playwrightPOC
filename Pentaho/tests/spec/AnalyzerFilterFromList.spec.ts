fe647680f85818f710cf9ae01a3aa0873bd09f71
JoaoEnesGoncalves <joao.goncalves@hitachivantara.com>
2023-06-02 15:22:07 +0100

    feat(test): organize tests into data driven

--- Pentaho/tests/spec/AnalyzerFilterFromList.spec.ts
+++ Pentaho/tests/spec/AnalyzerFilterFromList.spec.ts
@@ -12,33 +12,41 @@ const filters = ["Territory excludes APAC, Japan, NA and Not Available",
     "Territory includes EMEA"]
 const table = ['Territory', 'Sales', 'EMEA', '5,008,224']
 
-test('Filter select from a list - includes vs excludes', async ({ page }) => {
-    const reportEditMode = new ReportEditMode(page, pucReportPath)
 
-    await test.step(`Create report ${pucReportPath}`, async () => {
-        await report.createReport(pucReportPath, resourceReport)
+test.describe.configure({mode:"serial"})
+test.describe('Filter select from a list - includes vs excludes', () => {
+
+    test.beforeAll(async ({ }) => {
+        await test.step(`Create report ${pucReportPath}`, async () => {
+            await report.createReport(pucReportPath, resourceReport)
+        })
     })
 
     for (const index in filters) {
-        await test.step(`Given Admin user opened the Analyzer Report ${pucReportPath}"`, async () => {
-            await reportEditMode.goTo()
-        })
+        test(`Filter by text type for${filters[index]}`, async ({ page }) => {
+            const reportEditMode = new ReportEditMode(page, pucReportPath)
 
-        await test.step(`When Admin user filter the report by <FILTER>: ${filters[index]}`, async () => {
-            const tableSteps = new ReportTableSteps(page)
-            await tableSteps.setFilterAs(filters[index])
-        })
+            await test.step(`Given Admin user opened the Analyzer Report ${pucReportPath}"`, async () => {
+                await reportEditMode.goTo()
+            })
 
-        await test.step(`Then the report table should be`, async () => {
-            await reportEditMode.assertTableText(table)
+            await test.step(`When Admin user filter the report by: ${filters[index]}`, async () => {
+                const tableSteps = new ReportTableSteps(page)
+                await tableSteps.setFilterAs(filters[index])
+            })
+
+            await test.step(`Then the report table should be: ${table}`, async () => {
+                await reportEditMode.assertTableText(table)
+            })
+
         })
     }
 
-})
-
-
-test.afterAll(async ({ }) => {
-    await test.step(`delete report ${pucReportPath}`, async () => {
-        await report.deleteReport(pucReportPath)
+    test.afterAll(async ({ }) => {
+        await test.step(`delete report ${pucReportPath}`, async () => {
+            await report.deleteReport(pucReportPath)
+        })
     })
-})
+
+
+});
